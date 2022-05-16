5d9b8d16cf31688c6e3c00540b0890020f60803f
MarcelD234567890 <104261394+MarcelD234567890@users.noreply.github.com>
2022-05-16 06:27:58 +0200

    task und events mit mangelhafter Beschriftung

--- src/app/classes/diagram/elements/event.ts
+++ src/app/classes/diagram/elements/event.ts
@@ -32,5 +32,82 @@ export class Event extends Element {
     }
 
 
+    public getStartSvg(): SVGElement {
+        // // Startereignis
+        // const svg = this.createSvgElement('circle');
+        // svg.setAttribute('cx', `${this.x}`);
+        // svg.setAttribute('cy', `${this.y}`);
+        // svg.setAttribute('r', '50');
+        // svg.setAttribute('fill', 'none');
+        // svg.setAttribute('stroke', 'black');
+        // svg.setAttribute('stroke-width', "5");
+        // this.registerSvg(svg);
+
+        const svg = this.createSvgElement('svg');
+        svg.setAttribute('x', `${this.x}`);
+        svg.setAttribute('y', `${this.y}`);
+
+        const circle = this.createSvgElement('circle');
+        circle.setAttribute('r', '50');
+        circle.setAttribute('transform', 'translate(56 56)');
+        circle.setAttribute('fill', 'none');
+        circle.setAttribute('stroke', 'black');
+        circle.setAttribute('stroke-width', "3");
+        svg.append(circle);
+        this.registerSvg(svg);
+
+
+        return svg;
+    }
+
+    public getIntermediateSvg(): SVGElement {
+        // Zwischeneignis
+        const svg = this.createSvgElement('svg');
+        svg.setAttribute('x', `${this.x}`);
+        svg.setAttribute('y', `${this.y}`);
+
+
+        const circle = this.createSvgElement('circle');
+        circle.setAttribute('r', '50');
+        circle.setAttribute('transform', 'translate(56 56)');
+        circle.setAttribute('fill', 'none');
+        circle.setAttribute('stroke', 'black');
+        circle.setAttribute('stroke-width', "9");
+        svg.append(circle);
+
+        const circle2 = this.createSvgElement('circle');
+        circle2.setAttribute('r', '50');
+        circle2.setAttribute('transform', 'translate(56 56)');
+        circle2.setAttribute('fill', 'none');
+        circle2.setAttribute('stroke', 'white');
+        circle2.setAttribute('stroke-width', "3");
+        svg.append(circle2);
+        this.registerSvg(svg);
+        return svg;
+    }
+
+    public getEndSvg(): SVGElement {
+        // Endeereignis
+        const svg = this.createSvgElement('circle');
+        svg.setAttribute('x', `${this.x}`);
+        svg.setAttribute('y', `${this.y}`);
+
+        const circle = this.createSvgElement('circle');
+        svg.setAttribute('r', '50');
+        circle.setAttribute('transform', 'translate(56 56)');
+        circle.setAttribute('fill', 'none');
+        circle.setAttribute('stroke', 'black');
+        circle.setAttribute('stroke-width', "9");
+        svg.append(circle);
+        this.registerSvg(svg);
+        return svg;
+    }
+
+
+
+
+
+
+
 
 }
