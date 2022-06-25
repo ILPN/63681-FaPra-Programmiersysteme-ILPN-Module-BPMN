b11233468f5faeeefb1b02dc71c0c5d23b606929
Tata <tatiana.nikolaeva@gmx.net>
2022-06-25 20:29:46 +0200

    Uninstalled jest

--- src/tests/switch/or_gateway/simple-graph.test.ts
+++ src/tests/switch/or_gateway/simple-graph.test.ts
@@ -4,62 +4,95 @@ import { SwitchController } from "../../../../src/app/classes/Basic/Switch/switc
 import { SwitchableGraph } from "../../../../src/app/classes/Basic/Switch/SwitchableGraph";
 import { SwitchableNode } from "../../../../src/app/classes/Basic/Switch/SwitchableNode";
 import { SimpleOrGraph } from "../sample_graphs/simple-or-graph";
+import { TestBed, async } from '@angular/core/testing';
 
+// describe('Hello world', () => {
 
+//     let diagram: SwitchableGraph;
+//     let controller: SwitchController;
+//     //nodes
+//     let startEvent: SwitchableNode
+//     let task1: SwitchableNode
+//     let gatewaySplitOr1: SwitchableGateway
+//     let task2: SwitchableNode
+//     let task3: SwitchableNode
+//     let gatewayJoinOr1: SwitchableGateway
+//     let endEvent: SwitchableNode
+  
+//     beforeEach(() => {
+//         diagram = new SwitchableGraph(SimpleOrGraph.create())
+//         controller = diagram.controller
 
-describe('Simple graph with OR gateway', () => {
-    let diagram: SwitchableGraph;
-    let controller: SwitchController;
-
-    //nodes
-    let startEvent: SwitchableNode
-    let task1: SwitchableNode
-    let gatewaySplitOr1: SwitchableGateway
-    let task2: SwitchableNode
-    let task3: SwitchableNode
-    let gatewayJoinOr1: SwitchableGateway
-    let endEvent: SwitchableNode
-
-    beforeEach(() => {
-        diagram = new SwitchableGraph(SimpleOrGraph.create())
-        controller = diagram.controller
-
-        startEvent = diagram.getNode("StartEvent1")
-        task1 = diagram.getNode("Task1")
-        gatewaySplitOr1 = diagram.getNode("GatewaySplitOr1")
-        task2 = diagram.getNode("Task2")
-        task3 = diagram.getNode("Task3")
-        gatewayJoinOr1 = diagram.getNode("GatewayJoinOr1")
-        endEvent = diagram.getNode("EndEvent1")
-
-    })
-    test('Initial status of nodes when the diagram was initialized', () => {
-
-        expect(startEvent.switchState).toEqual(SwitchState.enableable)
-        expect(task1.switchState).toEqual(SwitchState.disabled)
-
-    });
-
-    test('Status of nodes when StartEvent is clicked', () => {
-
-        controller.press(startEvent)
-
-        expect(startEvent.switchState).toEqual(SwitchState.enabled)
-        expect(task1.switchState).toEqual(SwitchState.enableable)
-        expect(gatewayJoinOr1.switchState).toEqual(SwitchState.disabled)
-        expect(task2.switchState).toEqual(SwitchState.disabled)
-    });
-
-    test('Recursive search for preceding SPLIT gateway', () => {
-        controller.press(startEvent)
-        controller.press(task1)
-        controller.press(gatewaySplitOr1)
-        controller.press(task2)
-
-        let found: boolean = controller.recursivelySearchForResponsibleSplitGateway(task2, [])
-
-        expect(found).toEqual(false)
-       
-    });
+//         startEvent = diagram.getNode("StartEvent1")
+//         task1 = diagram.getNode("Task1")
+//         gatewaySplitOr1 = diagram.getNode("GatewaySplitOr1")
+//         task2 = diagram.getNode("Task2")
+//         task3 = diagram.getNode("Task3")
+//         gatewayJoinOr1 = diagram.getNode("GatewayJoinOr1")
+//         endEvent = diagram.getNode("EndEvent1")
+//     });
+  
     
-})
+  
+//     it('Initial status of nodes when the diagram was initialized', () => {
+//       expect(startEvent.switchState).toEqual(SwitchState.enableable)
+//       expect(task1.switchState).toEqual(SwitchState.disabled)
+//     });
+//   });
+
+// describe('Simple graph with OR gateway', () => {
+//     let diagram: SwitchableGraph;
+//     let controller: SwitchController;
+
+//     //nodes
+//     let startEvent: SwitchableNode
+//     let task1: SwitchableNode
+//     let gatewaySplitOr1: SwitchableGateway
+//     let task2: SwitchableNode
+//     let task3: SwitchableNode
+//     let gatewayJoinOr1: SwitchableGateway
+//     let endEvent: SwitchableNode
+
+//     beforeEach(() => {
+//         diagram = new SwitchableGraph(SimpleOrGraph.create())
+//         controller = diagram.controller
+
+//         startEvent = diagram.getNode("StartEvent1")
+//         task1 = diagram.getNode("Task1")
+//         gatewaySplitOr1 = diagram.getNode("GatewaySplitOr1")
+//         task2 = diagram.getNode("Task2")
+//         task3 = diagram.getNode("Task3")
+//         gatewayJoinOr1 = diagram.getNode("GatewayJoinOr1")
+//         endEvent = diagram.getNode("EndEvent1")
+
+//     })
+//     test('Initial status of nodes when the diagram was initialized', () => {
+
+//         expect(startEvent.switchState).toEqual(SwitchState.enableable)
+//         expect(task1.switchState).toEqual(SwitchState.disabled)
+
+//     });
+
+//     test('Status of nodes when StartEvent is clicked', () => {
+
+//         controller.press(startEvent)
+
+//         expect(startEvent.switchState).toEqual(SwitchState.enabled)
+//         expect(task1.switchState).toEqual(SwitchState.enableable)
+//         expect(gatewayJoinOr1.switchState).toEqual(SwitchState.disabled)
+//         expect(task2.switchState).toEqual(SwitchState.disabled)
+//     });
+
+//     test('Recursive search for preceding SPLIT gateway', () => {
+//         controller.press(startEvent)
+//         controller.press(task1)
+//         controller.press(gatewaySplitOr1)
+//         controller.press(task2)
+
+//         let found: boolean = controller.recursivelySearchForResponsibleSplitGateway(task2, [])
+
+//         expect(found).toEqual(false)
+       
+//     });
+    
+// })
